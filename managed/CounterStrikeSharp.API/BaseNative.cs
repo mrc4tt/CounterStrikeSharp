b9f8fcae9df9200ce0cab0e2237e948f990054af
mrc4tt <mikkel.linddk@gmail.com>
2026-01-20 20:36:14 +0100

    Test: memory

--- managed/CounterStrikeSharp.API/BaseNative.cs
+++ managed/CounterStrikeSharp.API/BaseNative.cs
@@ -15,20 +15,45 @@
  */
 
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace CounterStrikeSharp.API
 {
     public abstract class NativeObject
     {
-        public IntPtr Handle { get; internal set; }
+        private IntPtr _handle;
+
+        public IntPtr Handle
+        {
+            get
+            {
+                if (_handle == IntPtr.Zero)
+                {
+                    EnsureNativeHandle();
+                }
+
+                return _handle;
+            }
+            internal set => _handle = value;
+        }
+
+        internal IntPtr RawHandle => _handle;
 
         protected NativeObject(IntPtr pointer)
         {
-            Handle = pointer;
+            _handle = pointer;
+        }
+
+                protected void SetHandle(IntPtr pointer)
+        {
+            _handle = pointer;
+        }
+
+        protected virtual void EnsureNativeHandle()
+        {
         }
         
         /// <summary>
